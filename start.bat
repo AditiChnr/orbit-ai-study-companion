7e0d48b90c01364c7eefb75b6e7cd9fb54f84a6f
AditiChnr <106586634+AditiChnr@users.noreply.github.com>
2026-06-03 03:53:54 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -6,7 +6,7 @@ set "PYPATH=C:\Users\deepa\AppData\Local\Programs\Python\Launcher"
 set "PATH=%PATH%;%NODEPATH%;%PYPATH%"
 
 echo Starting ORBIT Backend...
-start "ORBIT Backend" cmd /k "cd /d C:\SmartStudy\backend && venv\Scripts\activate.bat && set GEMINI_API_KEY=AIzaSyDeS3AuFZXDzNvgEUoCMjgPvF3hsgUSSQ8 && py app.py"
+start "ORBIT Backend" cmd /k "cd /d C:\SmartStudy\backend && venv\Scripts\activate.bat && set GEMINI_API_KEY=YOUR_GEMINI_API_KEY_HERE && set MONGO_URI=YOUR_MONGO_URI_HERE && py app.py"
 
 echo Waiting for backend...
 timeout /t 5 /nobreak > nul
